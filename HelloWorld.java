c84412b9fca1c47d8e0eefec823549e4b9ef957a
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-02-12 13:01:07 -0700

    Added Hello World .java and .class files

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /**
+ * This function prints hello world when called
+ */
+private static void printHelloWorld() {
+    System.out.print("Hello World!");
+ }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
